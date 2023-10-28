eea33bc00137dd03df394c90c6c5adcbe497b191
Rahul balati <118408469+rahulbalati@users.noreply.github.com>
2023-10-28 18:31:25 +0530

    Update Student.java

--- static/Student.java
+++ static/Student.java
@@ -3,12 +3,12 @@ import java.util.Scanner;
 class Students{
     String name;
     int id;
-    static String clg="bkit";
-    Students(String name,int id){
+    static String clg="bkit";          //can be accessed directly
+    Students(String name,int id){      //setting name,id using constructor
         this.name=name;
         this.id=id;
     }
-    public String toString(){
+    public String toString(){          //overriding public toString Method
         return "student id: "+id+" Student name: "+name+" clg name: "+clg;
     }
 
@@ -22,7 +22,9 @@ public class Student {
             System.out.println("enter student id");
             int id=sc.nextInt();
             System.out.println("enter name");
-            String name=sc.next(); Students s=new Students(name,id);  System.out.println(s);
+            String name=sc.next(); 
+            Students s=new Students(name,id); 
+            System.out.println(s);
 
         
       
