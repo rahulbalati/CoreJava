6b8471f445475c6cb290078920decd06fae626bd
Rahul balati <118408469+rahulbalati@users.noreply.github.com>
2023-10-28 18:28:29 +0530

    Update StaticExp.java

--- static/StaticExp.java
+++ static/StaticExp.java
@@ -1,6 +1,6 @@
 class StaticExp{
     static{
-        System.out.println("static block printed");        //prints without creating an object
+        System.out.println("static block printed");        //prints without creating an object always static block executes first 
     }
    static void someMethod(){
     System.out.println("method called");
@@ -10,8 +10,8 @@ class StaticExp{
         System.out.println("constructor is called");
     }
     public static void main(String[] args) {
-        StaticExp s=new StaticExp();
-      someMethod();
+        StaticExp s=new StaticExp();                  // next constructor executes
+      someMethod();                                   //we can directly call the static method without creating an object
         
     }
 }
